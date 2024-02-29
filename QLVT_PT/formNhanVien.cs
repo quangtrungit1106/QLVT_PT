dfe35f15e069b5ca686a28f886494adf7b46b412
bao <vuquocbao673@gmail.com>
2024-02-29 21:20:50 +0700

    form nhân viên

---
+++ QLVT_PT/formNhanVien.cs
@@ -0,0 +1,254 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLVT_PT
+{
+    public partial class formNhanVien : Form
+    {
+        int vitri = 0;
+        string macn = "";
+        public formNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void nhanVienBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bdsNV.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.DS1);
+
+        }
+
+        private void formNhanVien_Load(object sender, EventArgs e)
+        {
+            DS1.EnforceConstraints = false;
+
+            this.nhanVienTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.nhanVienTableAdapter.Fill(this.DS1.NhanVien);
+
+            this.phieuXuatTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.phieuXuatTableAdapter.Fill(this.DS1.PhieuXuat);
+
+            this.datHangTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.datHangTableAdapter.Fill(this.DS1.DatHang);
+
+            this.phieuNhapTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.phieuNhapTableAdapter.Fill(this.DS1.PhieuNhap);
+
+            macn = ((DataRowView)bdsNV[0])["MACN"].ToString();
+            cmbChiNhanh.DataSource = Program.bindingSource; //sao chép bds đã load ở đăng nhập
+            cmbChiNhanh.DisplayMember = "TENCN";
+            cmbChiNhanh.ValueMember = "TENSERVER";
+            cmbChiNhanh.SelectedIndex = Program.brand;
+            if (Program.role == "CONGTY")
+            {
+                btnThem.Enabled = btnXoa.Enabled = btnSua.Enabled = btnGhi.Enabled = btnUndo.Enabled = false;
+                cmbChiNhanh.Enabled = true; //bật tắt theo phân quyền
+            }
+            else
+            {
+                btnThem.Enabled = btnXoa.Enabled = btnSua.Enabled = btnGhi.Enabled = btnUndo.Enabled = true;
+                cmbChiNhanh.Enabled = false;
+            }
+
+        }
+
+        private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            vitri = bdsNV.Position;
+            panelControl2.Enabled = true;
+            bdsNV.AddNew();
+            txtMACN.Text = macn;
+            dtNGAYSINH.EditValue = "";
+
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = btnReload.Enabled = btnThoat.Enabled = false;
+            btnGhi.Enabled = btnUndo.Enabled = true;
+            gcNhanVien.Enabled = false;
+        }
+
+        private void btnXoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Int32 manv = 0;
+            if (bdsDH.Count > 0)
+            {
+                MessageBox.Show("Không thể xóa nhân viên này vì đã lập đơn hàng", "", MessageBoxButtons.OK);
+                return;
+            }
+            if (bdsPN.Count > 0)
+            {
+                MessageBox.Show("Không thể xóa nhân viên này vì đã lập phiếu nhập", "", MessageBoxButtons.OK);
+                return;
+            }
+            if (bdsPX.Count > 0)
+            {
+                MessageBox.Show("Không thể xóa nhân viên này vì đã lập phiếu xuất", "", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có thực sự muốn xóa nhân viên này?", "Xác nhận", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                try
+                {
+                    manv = int.Parse(((DataRowView)bdsNV[bdsNV.Position])["MANV"].ToString());  //giữ lại kiểm tra khi bị lỗi xóa
+                    bdsNV.RemoveCurrent();     //xóa trên màn hình
+                    this.nhanVienTableAdapter.Connection.ConnectionString = Program.connstr;
+                    this.nhanVienTableAdapter.Update(this.DS1.NhanVien);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi xóa nhân viên. Bạn hãy thử xóa lại\n" + ex.Message, "", MessageBoxButtons.OK);
+                    this.nhanVienTableAdapter.Fill(this.DS1.NhanVien);
+                    bdsNV.Position = bdsNV.Find("MANV", manv);
+                    return;
+                }
+            }
+
+            if (bdsNV.Count == 0) btnXoa.Enabled = false;
+        }
+
+        private void btnSua_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            vitri = bdsNV.Position;
+            panelControl2.Enabled = true;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = btnReload.Enabled = btnThoat.Enabled = false;
+            btnGhi.Enabled = btnUndo.Enabled = true;
+            gcNhanVien.Enabled = false;
+        }
+
+        private void btnGhi_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (txtMANV.Text.Trim() == "")
+            {
+                MessageBox.Show("Mã nhân viên không được thiếu!", "", MessageBoxButtons.OK);
+                txtMANV.Focus();
+                return;
+            }
+            if (txtHO.Text.Trim() == "")
+            {
+                MessageBox.Show("Họ nhân viên không được thiếu!", "", MessageBoxButtons.OK);
+                txtHO.Focus();
+                return;
+            }
+            if (txtTEN.Text.Trim() == "")
+            {
+                MessageBox.Show("Tên nhân viên không được thiếu!", "", MessageBoxButtons.OK);
+                txtTEN.Focus();
+                return;
+            }
+            //if (dtNGAYSINH.Text.Trim() == "")
+            //{
+            //    MessageBox.Show("Ngày sinh nhân viên không được thiếu!", "", MessageBoxButtons.OK);
+            //    dtNGAYSINH.Focus();
+            //    return;
+            //}
+            //if (txtLUONG.Text.Trim() == "")
+            //{
+            //    MessageBox.Show("Lương nhân viên không được thiếu!", "", MessageBoxButtons.OK);
+            //    txtLUONG.Focus();
+            //    return;
+            //}
+            //if (int.Parse(txtLUONG.Text) < 4000000)
+            //{
+            //    MessageBox.Show("Lương nhân viên phải lớn hơn 4.000.000", "", MessageBoxButtons.OK);
+            //    txtLUONG.Focus();
+            //    return;
+            //}
+            //if (txtDIACHI.Text.Trim() == "")
+            //{
+            //    MessageBox.Show("Địa chỉ không được bỏ trống", "", MessageBoxButtons.OK);
+            //    txtDIACHI.Focus();
+            //    return;
+            //}
+
+            try
+            {
+                bdsNV.EndEdit();
+                bdsNV.ResetCurrentItem();
+                this.nhanVienTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.nhanVienTableAdapter.Update(this.DS1.NhanVien);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi ghi nhân viên: " + ex.Message, "", MessageBoxButtons.OK);
+                return;
+            }
+
+            gcNhanVien.Enabled = true;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = btnThoat.Enabled = true;
+            btnGhi.Enabled = btnUndo.Enabled = false;
+
+            panelControl2.Enabled = false;
+        }
+
+        private void btnUndo_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            bdsNV.CancelEdit();
+            if (btnThem.Enabled == false) bdsNV.Position = vitri;
+            panelControl2.Enabled = false;
+            gcNhanVien.Enabled = true;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = btnReload.Enabled = btnThoat.Enabled = true;
+            btnGhi.Enabled = btnUndo.Enabled = false;
+        }
+
+        private void btnReload_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            try
+            {
+                this.nhanVienTableAdapter.Fill(this.DS1.NhanVien);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi Reload:" + ex.Message, "", MessageBoxButtons.OK);
+                return;
+            }
+        }
+
+        private void btnThoat_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmbChiNhanh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbChiNhanh.SelectedValue.ToString() == "System.Data.DataRowView") return;
+            Program.serverName = cmbChiNhanh.ToString();
+
+            if (cmbChiNhanh.SelectedIndex != Program.brand)
+            {
+                Program.loginName = Program.remoteLogin;
+                Program.loginPassword = Program.remotePassword;
+            }
+            else
+            {
+                Program.loginName = Program.currentLogin;
+                Program.loginPassword = Program.currentPassword;
+            }
+
+            if (Program.KetNoi() == 0)
+            {
+                MessageBox.Show("Lỗi kết nối về chi nhánh mới", "", MessageBoxButtons.OK);
+            }
+            else
+            {
+                this.nhanVienTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.nhanVienTableAdapter.Fill(this.DS1.NhanVien);
+                this.datHangTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.datHangTableAdapter.Fill(this.DS1.DatHang);
+                this.phieuNhapTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.phieuNhapTableAdapter.Fill(this.DS1.PhieuNhap);
+                this.phieuXuatTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.phieuXuatTableAdapter.Fill(this.DS1.PhieuXuat);
+                macn = ((DataRowView)bdsNV[0])["MACN"].ToString();
+            }
+        }
+    }
+}
